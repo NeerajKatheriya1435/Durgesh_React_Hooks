e1ecff0a51b3179661c3facd24de665bf9f14aec
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-12-13 14:25:33 +0530

    useRef hook explained

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,30 @@
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
-
-  // const [name, setName] = useState("Neeraj Katheriya")
-  // const handleChange = () => {
-  //   if (name === "Neeraj Katheriya") {
-  //     setName("Durgesh First");
-  //   } else {
-  //     setName("Neeraj Katheriya");
-  //   }
-  // }
-  // const object1 =
-  // {
-  //   id: 1,
-  //   name: "Neeraj",
-  //   age: 34
-  // }
-  // const [obj, setObj] = useState(object1)
-  // const handleClear = () => {
-  //   setObj({ ...obj, name: "Tech-Mech", age: 89 })
-  // }
   const [count, setCount] = useState(0)
   const [color, setColor] = useState(0)
+  let durgesh = useRef(0)
+  let divref = useRef()
 
-  // Runs on every render
   useEffect(() => {
-    console.log("Hello Durgesh i am on every render");
+    durgesh.current = durgesh.current + 1;
+    console.log("I am on every render... ", durgesh.current)
   })
-
-  // Runs on only first render
-
   useEffect(() => {
-    console.log("This was called only on first render")
-  }, [])
+    // console.log(divref.current.style.backGroundColor = "red")
 
-  useEffect(() => {
-    console.log("This is called when count is changed")
-  }, [count])
-
-  useEffect(() => {
-    console.log("This is called when color is changed")
   }, [color])
-
-
-
-
   const handlClickColor = () => {
     setColor(color + 1);
+    divref.current.style.backgroundColor = "red";
   }
   const handlClickCount = () => {
-    setColor(count + 1);
+    setCount(count + 1);
   }
   return (
     <>
-      {/* <div className="container my-4">
-        {
-          // obj.map((element) => {
-          //   return ()
-          // })
-          <h1>Name:{obj.name} && Age: {obj.age} Id:{obj.id}</h1>
-        }
-        // <button className="btn btn-primary" onClick={handleClear}>
-        //   Click Me
-        // </button>
-      </div> */}
-      {/* UseEffect */}
       <h1>{count}</h1>
       <h2>{color}</h2>
       <button className="btn btn-primary" onClick={handlClickColor}>
@@ -75,7 +33,9 @@ function App() {
       <button className="btn btn-primary" onClick={handlClickCount}>
         Click Count
       </button>
-
+      <div className="container" ref={divref}>
+        You are a good student
+      </div>
     </>
   )
 }
