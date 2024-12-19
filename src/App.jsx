e2934b268e99dad82f01ae8a32ddeef88f2d90eb
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-12-19 12:40:39 +0530

    useCallBack Practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 
+import { useCallback, useState } from 'react'
 import './App.css'
-import Comp1 from './components/Comp1';
-import { CounterProvider } from './context/CounterContext';
+import Navbar from './Navbar'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const home = "hello";
+  // const getNewHome = () => {
+  //   return "Value";
+  // }
+  const getNewHome = useCallback(
+    () => {
+      return "hello" + count
+    },
+    [count]
+  )
+
   return (
-    <CounterProvider>
-      <Comp1 />
-    </CounterProvider>
+    <div className='text-center'>
+      <Navbar home={home} getNewHome={getNewHome} />
+      <h3 className="text-primary">
+        Hello this is my count : {count}
+      </h3>
+
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+        Click Count
+      </button>
+    </div >
   )
 }
 
---
+++ src/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { memo } from 'react'
+
+
+
+const Navbar = ({ home, getNewHome }) => {
+    console.log("Hello Navbar")
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/home">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">{home}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/about">About {getNewHome()}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default memo(Navbar)
