efbf38b86e2cfc24191bb198d08b3ab324421aae
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-12-19 13:43:38 +0530

    useMemo Explained

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 
-import { useCallback, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import './App.css'
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
-  const home = "hello";
-  // const getNewHome = () => {
-  //   return "Value";
-  // }
-  const getNewHome = useCallback(
-    () => {
-      return "hello" + count
-    },
-    [count]
-  )
 
+  const num = new Array(3000000).fill(0).map((_, i) => {
+    return {
+      index: i,
+      isMagical: i === 2900000
+    }
+  })
+
+  const [number, setNumber] = useState(num)
+  const magical = useMemo(() => number.find((i) => i.isMagical === true), [number])
   return (
-    <div className='text-center'>
-      <Navbar home={home} getNewHome={getNewHome} />
+    <div className='text-center my-4'>
+      {/* <Navbar /> */}
       <h3 className="text-primary">
-        Hello this is my count : {count}
+        Hello this is my count : {count} with magic: {magical.index}
       </h3>
 
-      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+      <button className="btn btn-primary" onClick={() => {
+        setCount(count + 1);
+        if (count == 10) {
+          setNumber(new Array(100000).fill(0).map((_, i) => {
+            return {
+              index: i,
+              isMagical: i === 19000
+            }
+          }))
+        }
+      }
+
+
+      }>
         Click Count
       </button>
     </div >
